834137000bda088b2df5f25cae0fba70860b48a3
Robertas Liekis <robertasliekis@gmail.com>
2021-03-10 16:38:07 +0200

    initial commit

--- src/components/InfoWindow.js
+++ src/components/InfoWindow.js
@@ -7,6 +7,22 @@ import { faTimes } from "@fortawesome/free-solid-svg-icons";
 import { setClickedFloor, setClickedRoom } from "../actions";
 
 function InfoWindow({ clickedFloor, clickedRoom, setClickedFloor, setClickedRoom }) {
+  const floorText = [
+    "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsa explicabo, alias a eum cumque vel praesentium dolores consequuntur debitis blanditiis perferendis tempore tenetur qui. Esse odio aperiam rerum reiciendis soluta.",
+    "Impedit, veniam dignissimos odit repudiandae iste id fugiat tenetur necessitatibus amet aspernatur voluptas dolorem deserunt dolores, reprehenderit minima! Asperiores,  praesentium delectus optio perspiciatis quae est ea.",
+    "Sit, amet consectetur adipisicing elit. Ipsa explicabo, alias a eum cumque vel praesentium dolores consequuntur debitis blanditiis perferendis tempore tenetur qui. Esse odio aperiam rerum reiciendis soluta.",
+    "Dolor sit, amet consectetur adipisicing elit. Ipsa explicabo, alias a eum cumque vel praesentium dolores consequuntur debitis blanditiis perferendis tempore tenetur qui. Esse odio aperiam rerum reiciendis soluta.",
+    "Adipisicing elit. Ipsa explicabo, alias a eum cumque vel praesentium dolores consequuntur debitis blanditiis perferendis tempore tenetur qui. Esse odio aperiam rerum reiciendis soluta."
+  ];
+  const roomText = [
+    "Ipsa explicabo, alias a eum cumque vel praesentium dolores consequuntur debitis blanditiis perferendis tempore tenetur qui. Esse odio aperiam rerum reiciendis soluta.",
+    "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsa explicabo, alias a eum cumque vel praesentium dolores consequuntur debitis blanditiis perferendis tempore tenetur qui. Esse odio aperiam rerum reiciendis soluta.",
+    "Impedit, veniam dignissimos odit repudiandae iste id fugiat tenetur necessitatibus amet aspernatur voluptas dolorem deserunt dolores, reprehenderit minima! Asperiores,  praesentium delectus optio perspiciatis quae est ea.",
+    "Sit, amet consectetur adipisicing elit. Ipsa explicabo, alias a eum cumque vel praesentium dolores consequuntur debitis blanditiis perferendis tempore tenetur qui. Esse odio aperiam rerum reiciendis soluta.",
+    "Dolor sit, amet consectetur adipisicing elit. Ipsa explicabo, alias a eum cumque vel praesentium dolores consequuntur debitis blanditiis perferendis tempore tenetur qui. Esse odio aperiam rerum reiciendis soluta.",
+    "Adipisicing elit. Ipsa explicabo, alias a eum cumque vel praesentium dolores consequuntur debitis blanditiis perferendis tempore tenetur qui. Esse odio aperiam rerum reiciendis soluta."
+  ];
+
   return (
     <div className="info-window" style={{ right: clickedFloor !== null ? "0%" : "-100%" }}>
       <div
@@ -20,17 +36,11 @@ function InfoWindow({ clickedFloor, clickedRoom, setClickedFloor, setClickedRoom
       </div>
       <div className="floor-container container">
         <h1>{`Floor ${clickedFloor + 1}`}</h1>
-        <p>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsa explicabo, alias a eum cumque vel praesentium dolores consequuntur
-          debitis blanditiis perferendis tempore tenetur qui. Esse odio aperiam rerum reiciendis soluta.
-        </p>
+        <p>{floorText[clickedFloor]}</p>
       </div>
       <div className="room-container container" style={{ display: clickedRoom !== null ? "flex" : "none" }}>
         <h1>{`Room ${clickedRoom + 1}`}</h1>
-        <p>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsa explicabo, alias a eum cumque vel praesentium dolores consequuntur
-          debitis blanditiis perferendis tempore tenetur qui. Esse odio aperiam rerum reiciendis soluta.
-        </p>
+        <p>{roomText[clickedRoom]}</p>
       </div>
     </div>
   );
